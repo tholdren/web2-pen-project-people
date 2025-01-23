fa1e3b25d375e055f45d4f68d07777dca488f098
Tristan Holdren <tristan.holdren@gmail.com>
2025-01-23 19:38:21 +0000

    Added Confetti

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("👍 Okay");
+let confetti = new Confetti('confetti');
+
+// options
+confetti.setCount(100);
+confetti.setPower(100);
+confetti.setSize(10);
+confetti.setFade(false);
+confetti.destroyTarget(false); 
+// console.log(confetti);
